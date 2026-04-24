12626a6c6a82ce5db80e45aa8f4369b41a873eff
Frankestein-XIII <khagendrad.4510@gmail.com>
2026-04-24 19:18:30 +0800

    fix:tsconfig-app fix for ci/cd issue

--- taskManager-client/jest.config.cjs
+++ taskManager-client/jest.config.cjs
@@ -2,6 +2,10 @@ module.exports ={
     preset:'ts-jest',
     testEnvironment:'jest-environment-jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.app.json'  // ← point to the real config
+    }]},
     moduleNameMapper:{
         '\\.(css|less|scss|sass)$':'identity-obj-proxy',
     },
