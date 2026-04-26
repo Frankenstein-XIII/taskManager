5fed96c7c86b19dde8e241f87f25f97fb5396cc0
Frankestein-XIII <khagendrad.4510@gmail.com>
2026-04-26 14:27:58 +0800

    fix: add test cases

--- taskManager-client/src/App.test.tsx
+++ taskManager-client/src/App.test.tsx
@@ -1,8 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App  from "./App";
+import api from './services/api';
 
-test("renders Vite + React Header", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Vite \+ React/i);
-  expect(linkElement).toBeInTheDocument();
+
+jest.mock("./services/api");
+const mockedApi = api as jest.Mocked<typeof api>;
+describe("App conponent test", () =>{
+  beforeEach(()=>{
+    jest.clearAllMocks();
+  });
+
+  test("renders Task Manager Header and loading state", async()=>{
+    mockedApi.get.mockResolvedValueOnce({data: []});
+    render(<App />);
+
+    //assert that the header text we created is visible
+    const headerElement = screen.getByText(/Task Manager 📝/i);
+    expect(headerElement).toBeInTheDocument();
+
+    const loadingElement = screen.getByText(/Loading your tasks/i);
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  test("renders seeded tasks after API call resolves ", async() =>{
+    mockedApi.get.mockResolvedValueOnce({
+      data: [
+        {
+          _id:'123',
+          title:'Test Task from Jest',
+          isCompleted:false,
+          createAt: new Date().toISOString()
+        }
+      ]
+    });
+    render(<App />);
+
+    await waitFor(() =>{
+      const taskElement = screen.getByText(/Test Task from Jest/i);
+      expect(taskElement).toBeInTheDocument();
+    });
+  });
 });
